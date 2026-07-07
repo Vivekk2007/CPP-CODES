1825259b231e4cffe366af5601635f6f8de81bd6
Vivekk2007 <devendrakumar29062007@gmail.com>
2026-07-07 09:45:33 +0530

    Voter ID VERIFICATION

---
+++ voterIDverification.cpp
@@ -0,0 +1,16 @@
+//IF ELSE STATMENT//
+
+#include <iostream>
+using namespace std;
+int main(){
+    int age;
+    cout<< "ENTER YOUR AGE: ";
+    cin>>age;
+    if(age>= 18){
+        cout<<"YOU ARE ELIGIBLE TO VOTE";
+    }
+    else{
+        cout<<"YOU ARE NOT ELIGIBLE TO VOTE "<<" Wait For "<< 18-age << "Years";
+    }
+    return 0;
+}
