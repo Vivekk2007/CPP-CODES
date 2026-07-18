9cf1c7bc7e1a28b4e51fe91e8a3574c10c5a3a31
Vivekk2007 <devendrakumar29062007@gmail.com>
2026-07-18 23:37:17 +0530

    Square of a number

---
+++ SquareofaNumber.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int num;
+    int square;
+
+    cout<<"Enter a number: ";
+    cin>>num;
+
+    square = num*num;
+    cout<<"Square of "<<num<<" is "<<square;
+
+    return 0;
+}
