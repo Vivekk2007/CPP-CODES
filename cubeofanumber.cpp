a106a160b3de168cef745637fbac74955e5b95bf
Vivekk2007 <devendrakumar29062007@gmail.com>
2026-07-19 22:19:39 +0530

    Cube of a Number

---
+++ cubeofanumber.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int num;
+    int cube;
+
+    cout<<"Enter a number: ";
+    cin>>num;
+
+    cube = num*num*num;
+    cout<<"Cube of "<<num<<" is "<<cube;
+
+    return 0;
+}
