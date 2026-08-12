44862942616a308befef3a7c153534d593b67cee
Vivekk2007 <devendrakumar29062007@gmail.com>
2026-08-12 22:47:19 +0530

    Traingle Pattern

---
+++ TranglePattern.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int n;
+    cout<<"Enter the number of Triangle:";
+    cin>>n;
+
+    int row=1;
+    while(row<=n){
+        int col =1;
+        while(col<=row){
+            cout<<"*"<<" ";
+            col++;
+        }
+    cout<<endl;
+    row++;
+    }
+
+}
